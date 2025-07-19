898f223bbf46a92ab674fdda719c455b4fe8c6ea
Katzover <100523537+Katzover@users.noreply.github.com>
2025-07-19 22:59:16 +0300

    dsadasda

--- script.js
+++ script.js
@@ -1572,12 +1572,3 @@ function showUsageGuide() {
 }
 
 if (!localStorage.getItem('lang')) {showUsageGuide();}
-function getOS() {
-  const userAgent = navigator.userAgent.toLowerCase();
-  if (userAgent.includes("android")) return "Android";
-  if (userAgent.includes("iphone") || userAgent.includes("ipad")) return "iOS";
-  if (userAgent.includes("win")) return "Windows";
-  if (userAgent.includes("mac")) return "MacOS";
-  if (userAgent.includes("linux")) return "Linux";
-  return "Unknown";
-} alert(getOS())
