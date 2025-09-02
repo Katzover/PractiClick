e630931787843a6ab440d3fd3f453ac1d40ac5a1
Katzover <100523537+Katzover@users.noreply.github.com>
2025-09-02 13:15:55 +0300

    עכגדעכגע

--- script.js
+++ script.js
@@ -530,7 +530,6 @@ async function autoReleaseStaleRooms() {
     if (error || !rooms) return;
 
     for (const room of rooms) {
-        console.log(isUserActive(room.username), rooms.username)
         if (room.status === "taken" && !isUserActive(room.username)) {
             await withLoading(() => updateRoomStatus(room.name, "available", 0));
         }
