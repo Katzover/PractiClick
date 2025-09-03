ea82e43f4c9300029e44f704844dca4a4a9310b9
Katzover <100523537+Katzover@users.noreply.github.com>
2025-09-03 21:51:16 +0300

    sadfafsasfda

--- script.js
+++ script.js
@@ -1459,7 +1459,7 @@ async function exportWeeklySummary() {
     const { error } = await supabase
         .from('summaries')
         .update({ json: getWeeklySummaryJson() }
-        .eq('username', localStorage.getItem('practiceUserName')));
+        .eq('username', await localStorage.getItem('practiceUserName')));
     if (error) {
         console.error('Failed to export weekly summary:', error.message);
         return;
