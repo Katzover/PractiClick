e83a77678990942d4a7987911e1060fffa065991
Katzover <100523537+Katzover@users.noreply.github.com>
2025-09-04 18:39:53 +0300

    gfdsgf

--- script.js
+++ script.js
@@ -2128,7 +2128,7 @@ async function checkforBook() {
     const bdate = data[0].date; 
     const now = new Date();
     console.log(bdate, now, 0, bdate == now, bdate < now, bdate > now);
-    if (bdate <= now) {
+    if (bdate.getTime() <= now.getTime()) {
         updateRoomStatus(data[0].what, data[0].why, 0);
     }
 }
